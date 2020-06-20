b0afd5585a7413af3f228ef21fe2e90dee189ffe
monkeyidesign <sam@monkeyidesign.com>
2020-06-20 21:16:25 +0700

    Adding array filter.

--- public/index.php
+++ public/index.php
@@ -6,12 +6,14 @@ $numbers = [1,2,3,4,5];
 //Loop solution
 $new_val_by_loop= [];
 foreach ($numbers as $number){
-    $new_val_by_loop[] = $number * 5;
+    if($number < 3){
+        $new_val_by_loop[] = $number;
+    }
 }
 var_dump($new_val_by_loop);
 
 //Array map solution
-$new_val_by_array_map = array_map(function ($data){
-    return $data * 5;
-}, $numbers);
+$new_val_by_array_map = array_filter($numbers, function ($data){
+    return $data < 3;
+});
 var_dump($new_val_by_array_map);
